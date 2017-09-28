0779db6bc357274d38592a1f495e12f7208783fd
Nicholas Inns <Nick.Inns@veritas.com>
2017-09-27 17:36:32 -0700

    plap

---
+++ PyTest.py
@@ -0,0 +1,39 @@
+def capital_case(x):
+    return x.capitalize()
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case2():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case3():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case4():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case5():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case6():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case7():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case8():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case9():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case10():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case11():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case12():
+    assert capital_case('semaphore') == 'Semaphore'
+
