248639f1e027f41224a43cabec3caa0081347203
Nicholas Inns <Nick.Inns@veritas.com>
2017-09-27 17:52:27 -0700

    Plap2

--- PyTest.py
+++ PyTest.py
@@ -1,39 +1,38 @@
 def capital_case(x):
     return x.capitalize()
 
-def test_capital_case():
+def test_case1():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case2():
+def test_case2():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case3():
+def test_case3():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case4():
+def test_case4():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case5():
+def test_case5():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case6():
+def test_case6():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case7():
+def test_case7():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case8():
+def test_case8():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case9():
+def test_case9():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case10():
+def test_case10():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case11():
+def test_case11():
     assert capital_case('semaphore') == 'Semaphore'
 
-def test_capital_case12():
+def test_case12():
     assert capital_case('semaphore') == 'Semaphore'
-
